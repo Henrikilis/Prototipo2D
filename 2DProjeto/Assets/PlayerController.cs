dcbd9f554bca9fc5da80463c8bc463a89047c232
Henrikilis <73853874+Henrikilis@users.noreply.github.com>
2021-02-02 16:11:04 -0300

    movimentacaoX

---
+++ 2DProjeto/Assets/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float moveSpeed;
+
+    private float inputX; 
+
+    void Start()
+    {
+    
+
+
+    }
+
+    
+    void Update()
+    {
+        rb.velocity = new Vector2(inputX * moveSpeed, rb.velocity.y);
+
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+
+        inputX = context.ReadValue<Vector2>().x;
+
+    }
+
+}
