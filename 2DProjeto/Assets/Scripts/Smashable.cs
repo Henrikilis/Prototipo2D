7f04f7ff31676568b717060bfa1859f0ad493245
Henrikilis <73853874+Henrikilis@users.noreply.github.com>
2021-02-09 15:51:19 -0300

    quicadaemobjetos

--- 2DProjeto/Assets/Scripts/Smashable.cs
+++ 2DProjeto/Assets/Scripts/Smashable.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Smashable : MonoBehaviour
 {
     public Animator anim;
+    public float stompJumpForce;
 
     // Start is called before the first frame update
     void Start()
@@ -23,9 +24,22 @@ public class Smashable : MonoBehaviour
         if(other.gameObject.CompareTag("PlayerAttack"))
         {
             anim.SetTrigger("Dead");
-            gameObject.layer = 0;
+            gameObject.layer = 13;
         }
+
+        if(other.GetComponent<Rigidbody2D>().velocity.y < 0)
+        {
+            other.GetComponent<Rigidbody2D>().velocity = new Vector2(other.GetComponent<Rigidbody2D>().velocity.x, 0);
+            other.GetComponent<Rigidbody2D>().AddForce(Vector2.up * stompJumpForce, ForceMode2D.Impulse);
+            anim.SetTrigger("Dead");
+            gameObject.layer = 13;
+
+        }
+
     }
+
+
+    
 
     public void Death()
     {
