d769ac5a9cecef3b86337a160c96e9316f57f676
Henrikilis <73853874+Henrikilis@users.noreply.github.com>
2021-04-22 15:26:33 -0300

    enemyWaypoints

---
+++ 2DProjeto/Assets/EnemyAI.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class EnemyAI : MonoBehaviour
+{
+
+    public Transform target;
+
+    public float speed = 200;
+    public float nxtwaypointdistance = 3;
+
+    Path path;
+    int currentWaypoint = 0;
+    bool reachendofpath = false;
+
+    Seeker seeker;
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        seeker = GetComponent<Seeker>();
+        rb = GetComponent<Rigidbody2D>();
+
+        seeker.StartPath(rb.position, target.position, OnPathComplete);
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    void OnPathComplete(Path p)
+    {
+
+
+    }
+
+}
---
+++ 2DProjeto/Assets/enemyGPX.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class enemyGPX : MonoBehaviour
+{
+    public AIPath aiPath;
+
+    private void Start()
+    {
+        aiPath = GetComponent<AIPath>();    
+    }
+
+    void Update()
+    {
+        if(aiPath.desiredVelocity.x  >= 0.01f)
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+
+        }
+        else  if (aiPath.desiredVelocity.x <= 0.01f)
+        {
+            transform.localScale = new Vector3(1f, 1f, 1f);
+
+        }
+    }
+}
