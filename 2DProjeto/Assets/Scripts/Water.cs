c2a339c4950791afaa433b0996abdc170e81980c
Gabriel Lo Monaco Honório <75794887+Gabriel-LF@users.noreply.github.com>
2021-04-23 15:29:18 -0300

    slash

---
+++ 2DProjeto/Assets/Scripts/Water.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using Debug = UnityEngine.Debug;
+
+public class Water : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        //if (other.gameObject.CompareTag("Player"))
+        //{
+            Debug.Log("Molhadinho");
+            ObjectPooler.Instance.SpawnFromPool("Splash", other.gameObject.transform.position, other.gameObject.transform.rotation);
+        //}
+    }
+}
