e63d389446bdc788bb019cabe6484a308ea08c2e
Henrikilis <73853874+Henrikilis@users.noreply.github.com>
2021-04-27 16:05:33 -0300

    InimigoAIBugado

--- 2DProjeto/Assets/EnemyAI.cs
+++ 2DProjeto/Assets/EnemyAI.cs
@@ -15,25 +15,71 @@ public class EnemyAI : MonoBehaviour
     int currentWaypoint = 0;
     bool reachendofpath = false;
 
-    Seeker seeker;
-    Rigidbody2D rb;
+   public  Seeker seeker;
+   public  Rigidbody2D rb;
 
     void Start()
     {
         seeker = GetComponent<Seeker>();
         rb = GetComponent<Rigidbody2D>();
 
-        seeker.StartPath(rb.position, target.position, OnPathComplete);
+        InvokeRepeating("updatePath", 0f, .5f);
+
+       // seeker.StartPath(rb.position, target.position, OnPathComplete);
     }
 
-    
-    void Update()
+    void updatePath()
     {
-        
+        if(seeker.IsDone())
+        seeker.StartPath(rb.position, target.position, OnPathComplete);
+
+    }
+    
+    void FixedUpdate()
+    {
+        if (path == null)
+            return;
+
+        if(currentWaypoint >= path.vectorPath.Count)
+        {
+            reachendofpath = true;
+            return;
+        } else
+        {
+            reachendofpath = false;
+        }
+
+        Vector2 direction = ((Vector2)path.vectorPath[currentWaypoint] - rb.position).normalized;
+        Vector2 force = direction * speed * Time.deltaTime;
+
+        rb.AddForce(force);
+
+        float distance = Vector2.Distance(rb.position, path.vectorPath[currentWaypoint]);
+
+        if (distance < nxtwaypointdistance)
+            currentWaypoint++;
+
+        if (force.x >= 0.01f)
+        {
+            transform.localScale = new Vector3(-2f, 2f, 2f);
+
+        }
+        else if (force.x <= 0.01f)
+        {
+            transform.localScale = new Vector3(2f, 2f, 2f);
+
+        }
+
     }
 
     void OnPathComplete(Path p)
     {
+        if (!p.error)
+        {
+            path = p;
+            currentWaypoint = 0;
+
+        }
 
 
     }
