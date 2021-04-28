804b4a9b017dd3d5d6dcdaa06c50e66c15d09890
Gabriel Lo Monaco Honório <75794887+Gabriel-LF@users.noreply.github.com>
2021-04-28 17:00:21 -0300

    BossBattle

---
+++ 2DProjeto/Assets/Scripts/DimensionalRune.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using Debug = UnityEngine.Debug;
+
+public class DimensionalRune : MonoBehaviour
+{
+    public DimensionSwap[] swapables;
+    // Start is called before the first frame update
+    void Start()
+    {
+        swapables = GameObject.FindObjectsOfType(typeof(DimensionSwap)) as DimensionSwap[];
+        StartCoroutine(Swap());
+    }
+
+    IEnumerator Swap()
+    {
+        yield return new WaitForSeconds(2);
+        //Debug.Log("Boss Trocou");
+        foreach (DimensionSwap target in swapables)
+        {
+            target.phased = !target.phased;
+            if (target.phased)
+                target.On();
+            else target.Off();
+        }
+        StartCoroutine(Swap());
+    }
+}
