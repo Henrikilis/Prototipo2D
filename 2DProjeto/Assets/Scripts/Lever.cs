1e0100abe2fba62192fb159c17a0c0921f322f3e
Gabriel Lo Monaco Honório <75794887+Gabriel-LF@users.noreply.github.com>
2021-04-29 17:05:44 -0300

    Levers

---
+++ 2DProjeto/Assets/Scripts/Lever.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lever : MonoBehaviour
+{
+    public Animator anim;
+    public GameObject target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("PlayerAttack"))
+        {
+            On();
+        }
+        if (other.gameObject.CompareTag("shield"))
+        {
+            On();
+        }
+    }
+
+    public void On()
+    {
+        anim.SetBool("On", true);
+        target.GetComponent<LeverTrigger>().On();
+    }
+}
---
+++ 2DProjeto/Assets/Scripts/LeverTrigger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeverTrigger : MonoBehaviour
+{
+    public Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    public void On()
+    {
+        anim.SetBool("On", true);
+        GetComponent<Collider2D>().isTrigger = true;
+    }
+}
