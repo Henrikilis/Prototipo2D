105061e663b510e6cfed5b69e32a431da453a9a9
Henrikilis <73853874+Henrikilis@users.noreply.github.com>
2021-06-18 17:06:30 -0300

    PowerUpHenriqueNaoPronto

---
+++ 2DProjeto/Assets/Scripts/HenriquePowerUp/PowerUpActions.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpActions : MonoBehaviour
+{
+  
+    
+    
+
+    public void IncreaseSpeed()
+    {
+       
+    }
+
+    public void IncreaseFireRate()
+    {      
+      
+    }
+    public void Heal()
+    {
+       
+    }
+    public void MoreShots()
+    {
+       
+    }
+}
